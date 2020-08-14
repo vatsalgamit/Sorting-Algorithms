45975c38db5e27ecd56a86a64f2fcf94b5d81dbf
Vatsal Gamit <35663138+vatsalgamit@users.noreply.github.com>
2020-08-14 11:43:53 +0530

    Add files via upload

---
+++ Insertion_Sort.py
@@ -0,0 +1,13 @@
+#Insertion Sort
+
+def insertion_sort(my_list):
+	for i in range(1,len(my_list)):
+		x = my_list[i]
+
+		while my_list[i-1]>x and i>0:
+			my_list[i],my_list[i-1]=my_list[i-1],my_list[i]
+			i = i-1
+	return my_list
+
+my_list = [2,5,8,1,2,3,5,6]
+print(insertion_sort(my_list))
